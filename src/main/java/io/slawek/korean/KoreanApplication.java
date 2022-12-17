a2e0437af31a7e3f83026aaabb16256f324298d0
Slawek Lysakowski <slawku1@gmail.com>
2022-12-17 22:18:32 +0100

    Initial commit

---
+++ src/main/java/io/slawek/korean/KoreanApplication.java
@@ -0,0 +1,16 @@
+package io.slawek.korean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+
+@SpringBootApplication
+@PropertySource("classpath:db.properties")
+
+public class KoreanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KoreanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/slawek/korean/KoreanApplicationTests.java
@@ -0,0 +1,13 @@
+package io.slawek.korean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KoreanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
